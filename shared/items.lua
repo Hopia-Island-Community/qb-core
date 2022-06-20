7b70fc228127e26cbb74572861306688417c46a6
Kevin Notari <kevin-notari@hotmail.fr>
2022-06-20 17:55:41 +0200

    ajout items paintball

--- shared/items.lua
+++ shared/items.lua
@@ -237,7 +237,14 @@ QBShared.Items = {
 
 	-- ITEMS
 
-	-- Ammo ITEMS
+    ['weapon_paintgun']     = {['name'] = 'weapon_paintgun',         ['label'] = 'Paint Ball Gun',        ['weight'] = 1000,         ['type'] = 'weapon',     ['ammotype'] = 'AMMO_PAINT',            ['image'] = 'weapon_paintgun.png',    ['unique'] = true,     ['useable'] = true,     ['description'] = 'Weapon Paint Ball'},
+
+    ['paint_ammo']			= {['name'] = 'paint_ammo', 			  	    ['label'] = 'Paint Ammo', 				['weight'] = 200, 		['type'] = 'item', 		['image'] = 'paint_ammo.png', 			['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,    ['combinable'] = nil,   ['description'] = 'Ammo for Paintball'},
+
+    ['weapon_paintgun']     = {['name'] = 'weapon_paintgun',    ['label'] = 'Paint Ball Gun',    ['ammotype'] = 'AMMO_PAINT',            ['damagereason'] = 'Painted'},
+
+
+-- Ammo ITEMS
 	['pistol_ammo'] 				 = {['name'] = 'pistol_ammo', 			  	  	['label'] = 'Munition pistolet', 	    ['weight'] = 200, 		['type'] = 'item', 		['image'] = 'pistol_ammo.png', 			['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,   ['combinable'] = nil,   ['description'] = 'Ammo for Pistols'},
 	['rifle_ammo'] 				 	 = {['name'] = 'rifle_ammo', 			  	    ['label'] = 'Munition fusil d\'assaut',	['weight'] = 1000, 		['type'] = 'item', 		['image'] = 'rifle_ammo.png', 			['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,   ['combinable'] = nil,   ['description'] = 'Ammo for Rifles'},
 	['smg_ammo'] 				 	 = {['name'] = 'smg_ammo', 			  	  		['label'] = 'Munition légère', 			['weight'] = 500, 		['type'] = 'item', 		['image'] = 'smg_ammo.png', 			['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,   ['combinable'] = nil,   ['description'] = 'Ammo for Sub Machine Guns'},
