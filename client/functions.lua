280588595171517376001cdbbafdc43037880936
GrumpySchlag <>
2022-12-25 23:32:05 +0100

    ENFIN bordel - adaptation de la fonction hasItem pour le nouvel inventaire

--- client/functions.lua
+++ client/functions.lua
@@ -12,6 +12,37 @@ function QBCore.Functions.GetCoords(entity)
     return vector4(coords.x, coords.y, coords.z, GetEntityHeading(entity))
 end
 
+function QBCore.Functions.HasItem(items, amount)
+    --return exports['qb-inventory']:HasItem(items, amount)
+    local isTable = type(items) == 'table'
+    local isArray = isTable and table.type(items) == 'array' or false
+    local totalItems = #items
+    local count = 0
+    local kvIndex = 2
+
+    if isTable and not isArray then totalItems = 0
+        for _ in pairs(items) do totalItems += 1 end
+        kvIndex = 1
+    end
+    for _, itemData in pairs(QBCore.Functions.GetPlayerData().items) do
+        if isTable then
+            for k, v in pairs(items) do
+                local itemKV = {k, v}
+                if itemData and itemData.name == itemKV[kvIndex] and ((amount and itemData.amount >= amount) and (not isArray and itemData.amount >=v) or (not amount and isArray)) then
+                    count += 1
+                end
+            end
+            if count == totalItems then
+                return true
+            end
+        else -- Single Items as String
+            if itemData and itemData.name == items and (not amount or (itemData and amount and itemData.amount >= amount)) then
+                return true
+            end
+        end
+    end
+end
+
 --function QBCore.Functions.HasItem(items, amount)
 --    return exports['qb-inventory']:HasItem(items, amount)
 --end
